80a70b0ae0dbe22877cc9cd92a6559dd3b10de39
noman-swe <noman.swe130@gmail.com>
2022-02-23 00:44:18 +0600

    32-2 Destructuring Object to extract values to variables

---
+++ destructuring.js
@@ -0,0 +1,21 @@
+const fish = {
+    id: 58,
+    name: 'King Hilsha',
+    price: 9000,
+    phone: '0131311642',
+    address: 'Chadpur',
+    dress: 'silver'
+};
+
+// const fishPhone = fish.phone;
+// const price = fish.price;
+// const dress = fish.dress;
+// const id = fish.id;
+
+// object elements variable declaretion system in short way
+const {phone, price, dress, id} = fish;
+
+console.log(phone);
+console.log(price);
+console.log(dress);
+console.log(id); 
