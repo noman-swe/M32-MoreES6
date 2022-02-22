46504891cbd591207a0fef7368fa8def57328580
noman-swe <noman.swe130@gmail.com>
2022-02-23 00:59:12 +0600

    32-3

---
+++ changing.js
@@ -0,0 +1,11 @@
+// declare variable based on the name of an object property
+const myObject = { x: 2, y: 50, z: 600, a: 25, b: 68 };
+const { x } = myObject;
+const { b } = myObject;
+console.log(x, b);
+
+// distructuring in array
+const [p, q] = [45, 27];
+console.log(p, q);
+
+// optional channing
