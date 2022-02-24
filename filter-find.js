231a80584de93eea0ecf2bb74f2634cdf0cfc968
noman-swe <noman.swe130@gmail.com>
2022-02-24 23:27:18 +0600

    32-6 (advanced) Implement filter, find on an array of objects

--- filter-find.js
+++ filter-find.js
@@ -2,8 +2,8 @@ const numbers = [5, 13, 7, 41, 30, 5, 2, 19];
 //The filter() method creates a new array filled with elements that pass a test provided by a function. The filter() method does not execute the function for empty elements. The filter() method does not change the original array.
 const bigNumbers = numbers.filter(num => num > 20);
 const smallNumbers = numbers.filter(num => num < 10);
-console.log(bigNumbers);
-console.log(smallNumbers);
+// console.log(bigNumbers);
+// console.log(smallNumbers);
 
 const products = [
     { name: 'Water bottle', price: 50, color: 'yellow' },
@@ -13,8 +13,18 @@ const products = [
     { name: 'water glass', price: 3, color: 'white' }
 ];
 const lowPrice = products.filter(product => product.price < 3000);
-console.log(lowPrice);
+// console.log(lowPrice);
 const expensive = products.filter(product => product.price > 100);
-console.log(expensive);
-const color = products.filter(pro => pro.color == 'black');
-console.log(color);
+// console.log(expensive);
+const colorBlack = products.filter(pro => pro.color == 'black');
+// console.log(color);
+
+
+//The find() method returns the value of the first element that passes a test. The find() method executes a function for each array element. The find() method retuns undefined if no elements are found.
+// find() just returns only the value or the element 
+const pinkColor = products.find(p => p.color == 'pink');
+console.log(pinkColor);
+
+const lowPrices = products.find(x => x.price > 500);
+console.log(lowPrices);
+
